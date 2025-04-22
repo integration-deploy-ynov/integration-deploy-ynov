57a95c9d2235a1c62b97f5697a484666c1d8a29e
Rei <60601546+Lukas-Bouhlel@users.noreply.github.com>
2025-04-22 11:40:41 +0200

    add api project

---
+++ backend/app/controllers/lamps.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const { Lamp } = require('../models');
+
+/**
+ * Récupérer toutes les difficultés.
+ */
+exports.readAll = async (req, res) => {
+    try {
+        const lamps = await Lamp.findAll();
+        res.status(200).json(lamps);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+/**
+ * Mettre à jour une difficulté existante.
+ */
+// Mettre à jour une lampe
+exports.update = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        // Récupérer la lampe par son ID
+        const lamp = await Lamp.findByPk(id);
+
+        if (!lamp) {
+            return res.status(404).json({ error: "Lampe non trouvée" });
+        }
+
+        // Inverser l'état de la lampe (true -> false, false -> true)
+        lamp.state = !lamp.state;
+        await lamp.save();
+
+        res.status(200).json({ message: 'Lampe mise à jour avec succès', lamp });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
---
+++ backend/app/migrations/20250422090940-create-lamps-table.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('lamps', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      state: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('lamps');
+  }
+};
